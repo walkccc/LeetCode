ef39cd6c2ed7b99f442b56ab6bff67ac5bb4b289
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-08-24 23:15:06 +0530

    Update 0973-3.cpp
    This code is much simpler and easy to understand then the previously written code
    This will help beginners to understand better as the length of code is less and simple concept is used.
    Thank You

--- solutions/0973. K Closest Points to Origin/0973-3.cpp
+++ solutions/0973. K Closest Points to Origin/0973-3.cpp
@@ -1,32 +1,17 @@
 class Solution {
- public:
-  vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-    quickSelect(points, 0, points.size() - 1, K);
-    return {begin(points), begin(points) + K};
-  };
-
- private:
-  void quickSelect(vector<vector<int>>& points, int l, int r, int K) {
-    const int randIndex = rand() % (r - l + 1) + l;
-    swap(points[randIndex], points[r]);
-    const vector<int> pivot = points[r];
-
-    int nextSwapped = l;
-    for (int i = l; i < r; ++i)
-      if (squareDist(points[i]) <= squareDist(pivot))
-        swap(points[nextSwapped++], points[i]);
-    swap(points[nextSwapped], points[r]);
-
-    const int count = nextSwapped - l + 1;  // # of points <= pivot
-    if (count == K)
-      return;
-    if (count > K)
-      quickSelect(points, l, nextSwapped - 1, K);
-    else
-      quickSelect(points, nextSwapped + 1, r, K - count);
-  }
-
-  int squareDist(const vector<int>& p) {
-    return p[0] * p[0] + p[1] * p[1];
-  }
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& p, int k) {
+        vector<vector<int>> kp;
+        priority_queue<vector<int>> pq;
+        for(int i=0;i<p.size();i++){
+            int d = p[i][0]*p[i][0] + p[i][1]*p[i][1];
+            pq.push({-1*d,p[i][0],p[i][1]});
+        }
+        for(int i=0;i<k;i++) {
+            vector<int> t = pq.top();pq.pop();
+            kp.push_back({t[1],t[2]});
+        }
+        
+        return kp;
+    }
 };

5946e247acde5b2b39cbfaea8fa1acf76637b972
Hunzlah Malik <50262751+crackaf@users.noreply.github.com>
2022-09-24 17:41:01 +0500

    Update 0007.cpp
    Added a solution using int which is 4bytes as required by the question.

--- solutions/0007. Reverse Integer/0007.cpp
+++ solutions/0007. Reverse Integer/0007.cpp
@@ -11,3 +11,23 @@ class Solution {
     return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
   }
 };
+
+class Solution
+{
+public:
+  // using int which is 4bytes, 32bits
+  int reverse(int x)
+  {
+    bool sign = x >= 0;
+    x = sign ? x : -x;
+    int ans = 0;
+    while (x > 0)
+    {
+      if (ans > INT_MAX / 10 - x % 10 + 1)
+        return 0;
+      ans = ans * 10 + x % 10;
+      x /= 10;
+    }
+    return sign ? ans : -ans;
+  }
+};

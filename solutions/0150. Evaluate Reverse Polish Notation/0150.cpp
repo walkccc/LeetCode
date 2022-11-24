6f88f817829d56a57cc7889128c12d72e6751e32
54_HAKS <ash091630sharma.54@gmail.com>
2022-11-24 21:23:35 +0530

    Changing 0150.cpp
    Previous Solution is getting Runtime Error, So Replacing it with working one.

--- solutions/0150. Evaluate Reverse Polish Notation/0150.cpp
+++ solutions/0150. Evaluate Reverse Polish Notation/0150.cpp
@@ -1,24 +1,19 @@
 class Solution {
- public:
-  int evalRPN(vector<string>& tokens) {
-    stack<int> stack;
-    const unordered_map<string, function<int(int, int)>> op{
-        {"+", plus<int>()},
-        {"-", minus<int>()},
-        {"*", multiplies<int>()},
-        {"/", divides<int>()}};
-
-    for (const string& token : tokens)
-      if (op.count(token)) {
-        const int b = stack.top();
-        stack.pop();
-        const int a = stack.top();
-        stack.pop();
-        stack.push(op.at(token)(a, b));
-      } else {
-        stack.push(stoi(token));
-      }
-
-    return stack.top();
-  }
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for(auto& i:tokens)
+            if (i=="+"||i=="-"||i=="*"||i=="/")
+            {
+                long int e1=s.top();s.pop();
+                long int e2=s.top();s.pop();
+                   if(i=="+")   e1=e2+e1;
+                    if(i=="-")  e1=e2-e1;
+                    if(i=="*")  e1=e2*e1;
+                    if(i=="/")  e1=e2/e1;
+                s.push(e1);
+            }
+            else s.push(stoi(i));         
+        return s.top();
+    }
 };

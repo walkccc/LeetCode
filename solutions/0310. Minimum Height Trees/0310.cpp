1e5334e336aea9f4ff2fa748d5a22fb601531dea
54_HAKS <ash091630sharma.54@gmail.com>
2023-01-10 14:05:40 +0530

    updating 0310.cpp
    Updating with Working Solution.

--- solutions/0310. Minimum Height Trees/0310.cpp
+++ solutions/0310. Minimum Height Trees/0310.cpp
@@ -1,35 +1,43 @@
 class Solution {
- public:
-  vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-    if (n == 1 || edges.empty())
-      return {0};
-
-    vector<int> ans;
-    unordered_map<int, unordered_set<int>> graph;
-
-    for (const vector<int>& edge : edges) {
-      const int u = edge[0];
-      const int v = edge[1];
-      graph[u].insert(v);
-      graph[v].insert(u);
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if(n==0)
+            return {};
+        if(n==1)
+            return {0};
+        vector<int>res;
+        vector<int>degrees(n,0);
+        vector<vector<int>>adj(n);
+        for(int i=0;i<edges.size();i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);//creating adjacent list
+            adj[edges[i][1]].push_back(edges[i][0]);
+            degrees[edges[i][1]]++;//updating how many edges each node has
+            degrees[edges[i][0]]++;
+        }
+        queue<int>queue;
+        for(int i=0;i<n;i++)
+        {
+            if(degrees[i]==1)//adding all the leave nodes
+                queue.push(i);
+        }
+        while(!queue.empty())
+        {
+            res.clear();// clear vector before we start traversing level by level.
+            int size=queue.size();
+            for(int i=0;i<size;i++)
+            {
+                int cur=queue.front();
+                queue.pop();
+                res.push_back(cur);//adding nodes to vector.Goal is to get a vector of  just 1 or 2 nodes available.
+                for(auto &neighbor:adj[cur])
+                {
+                    degrees[neighbor]--;//removing current leave nodes
+                    if(degrees[neighbor]==1)//adding current leave nodes
+                        queue.push(neighbor);
+                }
+            }
+        }
+        return res;
     }
-
-    for (const vector<int> & [ label, children ] : graph)
-      if (children.size() == 1)
-        ans.push_back(label);
-
-    while (n > 2) {
-      n -= ans.size();
-      vector<int> nextLeaves;
-      for (const int leaf : ans) {
-        const int u = *begin(graph[leaf]);
-        graph[u].erase(leaf);
-        if (graph[u].size() == 1)
-          nextLeaves.push_back(u);
-      }
-      ans = nextLeaves;
-    }
-
-    return ans;
-  }
 };

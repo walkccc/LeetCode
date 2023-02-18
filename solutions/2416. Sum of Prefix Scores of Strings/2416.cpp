371488578f4c722113fe2c94082baa104d4ec20f
beefidous <124520119+beefidous@users.noreply.github.com>
2023-02-18 17:20:55 +0100

    Fixed wrong answer for 2416

--- solutions/2416. Sum of Prefix Scores of Strings/2416.cpp
+++ solutions/2416. Sum of Prefix Scores of Strings/2416.cpp
@@ -1,44 +1,65 @@
-struct TrieNode {
-  vector<shared_ptr<TrieNode>> children;
-  int count = 0;
-  TrieNode() : children(26) {}
+class TrieNode {
+public:
+    vector<TrieNode*> child;
+    int availablePrefix;
 };
 
+const int CHILD_COUNT = 26;
+const int nax = 1e7 + 9;
+TrieNode pool[nax];
+int cnt;
+TrieNode *getNode() {
+    auto t = &pool[cnt++];
+    t->child.resize(CHILD_COUNT);
+    for (int i = 0; i < CHILD_COUNT; ++i) {
+        t->child[i] = nullptr;
+    }
+    t->availablePrefix = 0;
+    return t;
+}
+
 class Solution {
- public:
-  vector<int> sumPrefixScores(vector<string>& words) {
-    vector<int> ans;
-
-    for (const string& word : words)
-      insert(word);
-
-    for (const string& word : words)
-      ans.push_back(getScore(word));
-
-    return ans;
-  }
-
- private:
-  shared_ptr<TrieNode> root = make_shared<TrieNode>();
-
-  void insert(const string& word) {
-    shared_ptr<TrieNode> node = root;
-    for (const char c : word) {
-      const int i = c - 'a';
-      if (node->children[i] == nullptr)
-        node->children[i] = make_shared<TrieNode>();
-      node = node->children[i];
-      ++node->count;
+public:
+    TrieNode *root;
+    //this would insert all the prefixes of the current string
+    //in to the TRIE
+    void insert(const string &word) {
+        auto ptr = root;
+        for (char ch: word) {
+            int idx = ch - 'a';
+            if (!ptr->child[idx]) {
+                ptr->child[idx] = getNode();
+            }
+            ptr = ptr->child[idx];
+            ptr->availablePrefix += 1;
+        }
     }
-  }
-
-  int getScore(const string& word) {
-    shared_ptr<TrieNode> node = root;
-    int score = 0;
-    for (const char c : word) {
-      node = node->children[c - 'a'];
-      score += node->count;
+    
+    //This searches a string in TRIE and counts 
+    //all the prefixes along the path
+    int search (const string &word) {
+        auto ptr = root;
+        int prefixCount = 0;
+        for (char ch: word) {
+            int idx = ch - 'a';
+            if (!ptr->child[idx]) {
+                return prefixCount;
+            }
+            ptr = ptr->child[idx];
+            prefixCount += ptr->availablePrefix;
+        }
+        return prefixCount;
     }
-    return score;
-  }
+    vector<int> sumPrefixScores(vector<string>& words) {
+        cnt = 0;
+        root = getNode();
+        for (const string &s: words) {
+            insert(s);
+        }
+        vector<int> result;
+        for (const string &s: words) {
+            result.push_back(search(s));
+        }
+        return result;
+    }
 };

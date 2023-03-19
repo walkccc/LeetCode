62d091652b76198c1d0e8f402fcccdad325ac09b
54_HAKS <ash091630sharma.54@gmail.com>
2023-03-19 07:21:01 +0530

    Update 0211.py
    Last Solution was getting TLE. So, Updating with better one.

--- solutions/0211. Add and Search Word - Data structure design/0211.py
+++ solutions/0211. Add and Search Word - Data structure design/0211.py
@@ -1,33 +1,32 @@
 class TrieNode:
-  def __init__(self):
-    self.children: Dict[str, TrieNode] = collections.defaultdict(TrieNode)
-    self.isWord = False
-
-
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        
 class WordDictionary:
-  def __init__(self):
-    self.root = TrieNode()
+    def __init__(self):
+        self.root = TrieNode()      
 
-  def addWord(self, word: str) -> None:
-    node: TrieNode = self.root
-    for c in word:
-      if c not in node.children:
-        node.children[c] = TrieNode()
-      node = node.children[c]
-    node.isWord = True
+    def addWord(self, word):
+        current_node = self.root
+        for character in word:
+            current_node = current_node.children.setdefault(character, TrieNode())
+        current_node.is_word = True
+        
+    def search(self, word):
+        def dfs(node, index):
+            if index == len(word):
+                return node.is_word
+               
+            if word[index] == ".":
+                for child in node.children.values():
+                    if dfs(child, index+1):
+                        return True
+                    
+            if word[index] in node.children:
+                return dfs(node.children[word[index]], index+1)
+            
+            return False
+    
+        return dfs(self.root, 0)
 
-  def search(self, word: str) -> bool:
-    return self._dfs(word, 0, self.root)
-
-  def _dfs(self, word: str, s: int, node: TrieNode) -> bool:
-    if s == len(word):
-      return node.isWord
-    if word[s] != '.':
-      next: TrieNode = node.children[word[s]]
-      return self._dfs(word, s + 1, next) if next else False
-
-    for c in string.ascii_lowercase:
-      if c in node.children and self._dfs(word, s + 1, node.children[c]):
-        return True
-
-    return False

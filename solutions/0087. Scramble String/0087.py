d34f1923f22379797d5348ec1dcdba3cbf010ad0
54_HAKS <ash091630sharma.54@gmail.com>
2023-03-30 12:08:36 +0530

    Changing 0087.py
    previous Solution was getting TLE. So, changing with a better one.

--- solutions/0087. Scramble String/0087.py
+++ solutions/0087. Scramble String/0087.py
@@ -1,16 +1,19 @@
 class Solution:
-  def isScramble(self, s1: str, s2: str) -> bool:
-    if s1 == s2:
-      return True
-    if len(s1) != len(s2):
-      return False
-    if collections.Counter(s1) != collections.Counter(s2):
-      return False
+    def isScramble(self,s1, s2):
+        m ={}
+        def func(s1, s2):
+            if (s1, s2) in m:
+                return m[(s1, s2)]
+            if not sorted(s1) == sorted(s2):
+                return False
+            if len(s1) == 1:
+                return True
+            
 
-    for i in range(1, len(s1)):
-      if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]):
-        return True
-      if self.isScramble(s1[:i], s2[len(s2) - i:]) and self.isScramble(s1[i:], s2[:len(s2) - i]):
-        return True
-
-    return False
+            for i in range(1, len(s1)):
+                if func(s1[:i], s2[-i:]) and func(s1[i:], s2[:-i]) or func(s1[:i], s2[:i]) and func(s1[i:], s2[i:]):
+                    m[(s1, s2)] = True
+                    return True
+            m[(s1, s2)] = False
+            return False
+        return func(s1, s2)

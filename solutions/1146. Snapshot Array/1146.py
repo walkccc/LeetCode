fcdf8c2b2942b2a95222cf69f95c68379d1b2ac5
Tursunov Temirlan <36619221+su5kk@users.noreply.github.com>
2023-06-12 01:18:46 +0300

    Add some commentary 1146.py
    Decided to add some commentary to this task so that it can be kinda understandable without looking the editorial. If this is not needed, please let me know and close this PR

--- solutions/1146. Snapshot Array/1146.py
+++ solutions/1146. Snapshot Array/1146.py
@@ -1,10 +1,13 @@
 class SnapshotArray:
   def __init__(self, length: int):
+    # index -> array of (version, value)
     self.snaps = [[[0, 0]] for _ in range(length)]
     self.snap_id = 0
 
   def set(self, index: int, val: int) -> None:
     snap = self.snaps[index][-1]
+    # if the snapshot at the current idx
+    # is the most up to date one.
     if snap[0] == self.snap_id:
       snap[1] = val
     else:
@@ -15,5 +18,12 @@ class SnapshotArray:
     return self.snap_id - 1
 
   def get(self, index: int, snap_id: int) -> int:
+    # binary search on snap_id
+    # for a particular index,
+    # the snap ids s_i and s_j, s_i < s_j and i < j, are not necessarily in the form
+    # of s_i = s_j + 1
+    # see ["SnapshotArray","set","snap","set","get"] [[3],[0,5],[],[0,6],[0,0]]
+    # testcase for example.
+    # so it follows that we cannot do self.snaps[index][snap_id] right away.
     i = bisect_left(self.snaps[index], [snap_id + 1]) - 1
     return self.snaps[index][i][1]

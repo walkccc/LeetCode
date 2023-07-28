3ce361375c3cacc52fd24b0eb9eeecef615ce2c3
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-07-28 13:06:46 +0530

    Update 0473.cpp
    Way more optimized.

--- solutions/0473. Matchsticks to Square/0473.cpp
+++ solutions/0473. Matchsticks to Square/0473.cpp
@@ -1,32 +1,26 @@
 class Solution {
- public:
-  bool makesquare(vector<int>& matchsticks) {
-    if (matchsticks.size() < 4)
-      return false;
-
-    const int perimeter = accumulate(matchsticks.begin(), matchsticks.end(), 0);
-    if (perimeter % 4 != 0)
-      return false;
-
-    sort(matchsticks.begin(), matchsticks.end(), greater<int>());
-    return dfs(matchsticks, 0, vector<int>(4, perimeter / 4));
-  }
-
- private:
-  bool dfs(const vector<int>& matchsticks, int selected, vector<int>&& edges) {
-    if (selected == matchsticks.size())
-      return all_of(edges.begin(), edges.end(),
-                    [](int edge) { return edge == 0; });
-
-    for (int i = 0; i < 4; ++i) {
-      if (matchsticks[selected] > edges[i])
-        continue;
-      edges[i] -= matchsticks[selected];
-      if (dfs(matchsticks, selected + 1, move(edges)))
-        return true;
-      edges[i] += matchsticks[selected];
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        int s = 0, mx = 0;
+        for (int& v : matchsticks) {
+            s += v;
+            mx = max(mx, v);
+        }
+        int x = s / 4, mod = s % 4;
+        if (mod != 0 || x < mx) return false;
+        sort(matchsticks.begin(), matchsticks.end(), greater<int>());
+        vector<int> edges(4);
+        return dfs(0, x, matchsticks, edges);
     }
 
-    return false;
-  }
+    bool dfs(int u, int x, vector<int>& matchsticks, vector<int>& edges) {
+        if (u == matchsticks.size()) return true;
+        for (int i = 0; i < 4; ++i) {
+            if (i > 0 && edges[i - 1] == edges[i]) continue;
+            edges[i] += matchsticks[u];
+            if (edges[i] <= x && dfs(u + 1, x, matchsticks, edges)) return true;
+            edges[i] -= matchsticks[u];
+        }
+        return false;
+    }
 };

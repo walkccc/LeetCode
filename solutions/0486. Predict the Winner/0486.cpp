c61fed37be51decfd5236743ce191d4628f1c435
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-07-28 13:03:14 +0530

    Update 0486.cpp
    More optimized

--- solutions/0486. Predict the Winner/0486.cpp
+++ solutions/0486. Predict the Winner/0486.cpp
@@ -1,19 +1,18 @@
 class Solution {
- public:
-  bool PredictTheWinner(vector<int>& nums) {
-    const int n = nums.size();
-    // dp[i][j] := max number you can get more than your opponent in nums[i..j]
-    vector<vector<int>> dp(n, vector<int>(n));
-
-    for (int i = 0; i < n; ++i)
-      dp[i][i] = nums[i];
-
-    for (int d = 1; d < n; ++d)
-      for (int i = 0; i + d < n; ++i) {
-        const int j = i + d;
-        dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
-      }
-
-    return dp[0][n - 1] >= 0;
-  }
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        int f[n][n];
+        memset(f, 0, sizeof(f));
+        function<int(int, int)> dfs = [&](int i, int j) -> int {
+            if (i > j) {
+                return 0;
+            }
+            if (f[i][j]) {
+                return f[i][j];
+            }
+            return f[i][j] = max(nums[i] - dfs(i + 1, j), nums[j] - dfs(i, j - 1));
+        };
+        return dfs(0, n - 1) >= 0;
+    }
 };

0c8b1597c98830d3684c23af3c5b416fc88bce88
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-07-29 14:11:43 +0530

    Update 0808.cpp
    Optimized.

--- solutions/0808. Soup Servings/0808.cpp
+++ solutions/0808. Soup Servings/0808.cpp
@@ -1,23 +1,16 @@
 class Solution {
- public:
-  double soupServings(int n) {
-    return n >= 4800 ? 1.0 : dfs((n + 24) / 25, (n + 24) / 25);
-  }
-
- private:
-  vector<vector<double>> memo =
-      vector<vector<double>>(4800 / 25, vector<double>(4800 / 25));
-
-  double dfs(int a, int b) {
-    if (a <= 0 && b <= 0)
-      return 0.5;
-    if (a <= 0)
-      return 1.0;
-    if (b <= 0)
-      return 0.0;
-    if (memo[a][b] > 0)
-      return memo[a][b];
-    return memo[a][b] = 0.25 * (dfs(a - 4, b) + dfs(a - 3, b - 1) +
-                                dfs(a - 2, b - 2) + dfs(a - 1, b - 3));
-  }
+public:
+    double soupServings(int n) {
+        double f[200][200] = {0.0};
+        function<double(int, int)> dfs = [&](int i, int j) -> double {
+            if (i <= 0 && j <= 0) return 0.5;
+            if (i <= 0) return 1;
+            if (j <= 0) return 0;
+            if (f[i][j] > 0) return f[i][j];
+            double ans = 0.25 * (dfs(i - 4, j) + dfs(i - 3, j - 1) + dfs(i - 2, j - 2) + dfs(i - 1, j - 3));
+            f[i][j] = ans;
+            return ans;
+        };
+        return n > 4800 ? 1 : dfs((n + 24) / 25, (n + 24) / 25);
+    }
 };

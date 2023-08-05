2c27b7dadddce54919f539c6b69d63fc744dd5c5
54_HAKS <ash091630sharma.54@gmail.com>
2023-08-05 19:02:35 +0530

    Update 0065.cpp
    The answer is wrong. So changing with a working one! :)

--- solutions/0065. Valid Number/0065.cpp
+++ solutions/0065. Valid Number/0065.cpp
@@ -1,47 +1,54 @@
-class Solution {
- public:
-  bool isNumber(string s) {
-    trim(s);
-    if (s.empty())
-      return false;
-
-    bool seenNum = false;
-    bool seenDot = false;
-    bool seenE = false;
-
-    for (int i = 0; i < s.length(); ++i) {
-      switch (s[i]) {
-        case '.':
-          if (seenDot || seenE)
-            return false;
-          seenDot = true;
-          break;
-        case 'e':
-        case 'E':
-          if (seenE || !seenNum)
-            return false;
-          seenE = true;
-          seenNum = false;
-          break;
-        case '+':
-        case '-':
-          if (i > 0 && s[i - 1] != 'e' && s[i - 1] != 'E')
-            return false;
-          seenNum = false;
-          break;
-        default:
-          if (!isdigit(s[i]))
-            return false;
-          seenNum = true;
-      }
+class Solution 
+{
+public:
+    bool isNumber(string s)
+    {
+        bool digitSeen=false,dotSeen=false,eSeen=false;
+        int plusMinus=0,n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]-'0'>=0 && s[i]-'0'<=9)
+            {
+                digitSeen=true;
+            }
+            else if(s[i]=='+' || s[i]=='-')
+            {
+                if(plusMinus==2 || (i>0 && (s[i-1]!='e' && s[i-1]!='E')) || i==n-1)
+                {
+                    return false;
+                }
+                plusMinus++;
+            }
+            else if(s[i]=='e' || s[i]=='E')
+            {
+                if(eSeen || !digitSeen || i==n-1)
+                {
+                    return false;
+                }
+                eSeen=true;
+            }
+            else if(s[i]=='.')
+            {
+                if(eSeen || dotSeen || (i==n-1 && !digitSeen))
+                {
+                    return false;
+                }
+                dotSeen=true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
-    return seenNum;
-  }
 
- private:
-  void trim(string& s) {
-    s.erase(0, s.find_first_not_of(' '));
-    s.erase(s.find_last_not_of(' ') + 1);
-  }
+
+
+
+
+
+
+
 };

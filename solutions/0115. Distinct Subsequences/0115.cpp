6f8abc848a25c1857f03db411f453d09e21a814f
54_HAKS <ash091630sharma.54@gmail.com>
2023-08-05 19:10:34 +0530

    Update 0115.cpp
    Both Cpp solution are getting errors during Run. Updating it with different solution!

--- solutions/0115. Distinct Subsequences/0115.cpp
+++ solutions/0115. Distinct Subsequences/0115.cpp
@@ -1,20 +1,14 @@
 class Solution {
  public:
-  int numDistinct(string s, string t) {
-    const int m = s.length();
-    const int n = t.length();
-    vector<vector<unsigned long>> dp(m + 1, vector<unsigned long>(n + 1));
-
-    for (int i = 0; i <= m; ++i)
-      dp[i][0] = 1;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (s[i - 1] == t[j - 1])
-          dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-        else
-          dp[i][j] = dp[i - 1][j];
-
-    return dp[m][n];
-  }
+int numDistinct(string s, string t) {
+        int n = s.length(), m = t.length();
+        vector<double>dp(m+1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = m; j > 0; j--) {
+                if(s[i-1] == t[j-1]) dp[j] = dp[j-1] + dp[j];
+            }
+        }
+        return dp[m];    
+    }
 };

ca8d5ff465550da2e13ccb7726531f4a6252143e
Rahul Lohra <77195427+rahullo@users.noreply.github.com>
2023-08-13 17:28:16 +0530

    Update 0045.py
    Making the code simple

--- solutions/0045. Jump Game II/0045.py
+++ solutions/0045. Jump Game II/0045.py
@@ -1,17 +1,14 @@
 class Solution:
   def jump(self, nums: List[int]) -> int:
-    ans = 0
-    end = 0
-    farthest = 0
+    res = 0
+    r = l = 0
 
-    # Implicit BFS
-    for i in range(len(nums) - 1):
-      farthest = max(farthest, i + nums[i])
-      if farthest >= len(nums) - 1:
-        ans += 1
-        break
-      if i == end:      # Visited all the items on the current level
-        ans += 1        # Increment the level
-        end = farthest  # Make the queue size for the next level
-
-    return ans
+    while r < len(nums) - 1:
+        farthest = 0
+        for i in range(l, r+1):
+            farthest = max(farthest, i + nums[i])
+        
+        l = r+1
+        r = farthest
+        res +=1
+    return res

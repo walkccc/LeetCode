ad6b655f69a45be3ae7bc4e57dad3c4985279c5b
Shanukumar Singh <91169674+Shanu2409@users.noreply.github.com>
2023-10-29 13:52:44 +0530

    Update 0458.cpp
    Old code was Not full filling all the test cases. so i changed the logic a bit.
    
    not its passing all the test cases.

--- solutions/0458. Poor Pigs/0458.cpp
+++ solutions/0458. Poor Pigs/0458.cpp
@@ -1,6 +1,9 @@
 class Solution {
- public:
-  int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-    return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
-  }
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int base = minutesToTest / minutesToDie + 1;
+        int res = 0;
+        for (int p = 1; p < buckets; p *= base) ++res;
+        return res;
+    }
 };

80b8c4d5c654869b353dd4435cc293e39e40bbfe
Subhanshu Singh <112849297+Singh-ss@users.noreply.github.com>
2024-04-21 21:34:55 +0530

    Update 1781.java

--- solutions/1781. Sum of Beauty of All Substrings/1781.java
+++ solutions/1781. Sum of Beauty of All Substrings/1781.java
@@ -1,24 +1,20 @@
 class Solution {
-  public int beautySum(String s) {
-    int ans = 0;
-
-    for (int i = 0; i < s.length(); ++i) {
-      int[] count = new int[26];
-      for (int j = i; j < s.length(); ++j) {
-        ++count[s.charAt(j) - 'a'];
-        ans += Arrays.stream(count).max().getAsInt() - getMinFreq(count);
-      }
+    public int beautySum(String s) {
+        int beautySum=0;
+        for(int i=0;i<s.length();++i){
+            int[] count=new int[26];
+            for(int j=i;j<s.length();++j){
+                ++count[s.charAt(j)-'a'];
+                int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE;
+                for(int k=0;k<26;++k){
+                    if(count[k]!=0){
+                        max=Math.max(count[k],max);
+                        min=Math.min(count[k],min);
+                    }
+                }
+                beautySum+=max-min;
+            }
+        }
+        return beautySum;
     }
-
-    return ans;
-  }
-
-  // Returns the minimum frequency > 0.
-  private int getMin(int[] count) {
-    int minFreq = Integer.MAX_VALUE;
-    for (final int freq : count)
-      if (freq > 0)
-        minFreq = min(minFreq, freq);
-    return minFreq;
-  }
 }

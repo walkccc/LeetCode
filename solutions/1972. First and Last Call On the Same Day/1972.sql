eb496526a730431e12cfb1b660ac26dade1c9e39
Duc M. Quoc <1174046+ducquoc@users.noreply.github.com>
2025-01-24 19:37:02 +0700

    Update 1972.sql
    fix typo

--- solutions/1972. First and Last Call On the Same Day/1972.sql
+++ solutions/1972. First and Last Call On the Same Day/1972.sql
@@ -12,7 +12,7 @@ WITH
       RANK() OVER(
         PARTITION BY caller_id, DATE(call_time)
         ORDER BY call_time ASC
-      ) AS rank_asc
+      ) AS rank_asc,
       RANK() OVER(
         PARTITION BY caller_id, DATE(call_time)
         ORDER BY call_time DESC

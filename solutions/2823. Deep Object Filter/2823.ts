50bbeca19cdb833885566a5ee90b5edceb3c75e1
Duc M. Quoc <ducquoc.vn@gmail.com>
2025-01-29 22:07:53 +0700

    Update 2823.ts
    fix passed all testcases.

--- solutions/2823. Deep Object Filter/2823.ts
+++ solutions/2823. Deep Object Filter/2823.ts
@@ -8,21 +8,25 @@ type JSONValue =
 type Obj = Record<string, JSONValue> | Array<JSONValue>;
 
 function deepFilter(obj: Obj, fn: Function): Obj | undefined {
-  if (obj === null || typeof obj !== 'object') {
-    return fn(obj) ? obj : undefined;
-  }
-  if (Array.isArray(obj)) {
-    const ans = (obj as Obj[])
-      .map((item) => deepFilter(item, fn))
-      .filter((item) => item !== undefined);
-    return ans.length > 0 ? ans : undefined;
-  }
-  let ans = {};
-  for (const [key, value] of Object.entries(obj)) {
-    const ans = deepFilter(value as Obj, fn);
-    if (ans !== undefined) {
-      ans[key] = ans;
+  const dfs = (data: any): any => {
+    if (Array.isArray(data)) {
+      const res = data.map(dfs).filter((item: any) => item !== undefined);
+      return res.length > 0 ? res : undefined;
     }
-  }
-  return Object.keys(ans).length > 0 ? ans : undefined;
+    if (typeof data === 'object' && data !== null) {
+      const res: Record<string, any> = {};
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const filteredValue = dfs(data[key]);
+          if (filteredValue !== undefined) {
+            res[key] = filteredValue;
+          }
+        }
+      }
+      return Object.keys(res).length > 0 ? res : undefined;
+    }
+    return fn(data) ? data : undefined;
+  };
+
+  return dfs(obj);
 }

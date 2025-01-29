eb85d37d0e52f08c1dfb8386100c69291d5d5f50
Duc M. Quoc <ducquoc.vn@gmail.com>
2025-01-29 19:17:06 +0700

    Create 2036-2.java
    passed all testcases (85+2) efficiently, TC: O(N).

---
+++ solutions/2036. Maximum Alternating Subarray Sum/2036-2.java
@@ -0,0 +1,14 @@
+class Solution {
+  public long maximumAlternatingSubarraySum(int[] nums) {
+    final long inf = 1L << 60;
+    long ans = -inf, f = -inf, g = -inf;
+    for (int x : nums) {
+      long ff = Math.max(g, 0) + x;
+      g = f - x;
+      f = ff;
+      ans = Math.max(ans, Math.max(f, g));
+    }
+
+    return ans;
+  }
+}

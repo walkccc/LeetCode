b02ceb5a73c4687d01859110601740d7152fd7fb
JoyadityaNath <joyadityan@gmail.com>
2025-05-13 21:11:58 +0530

    Code Correction in 2110.java
    Changed prices.size() to prices.length inside the 'for' loop as 'prices' is an array not a List.

--- solutions/2110. Number of Smooth Descent Periods of a Stock/2110.java
+++ solutions/2110. Number of Smooth Descent Periods of a Stock/2110.java
@@ -3,7 +3,7 @@ class Solution {
     long ans = 1; // prices[0]
     int dp = 1;
 
-    for (int i = 1; i < prices.size(); ++i) {
+    for (int i = 1; i < prices.length; ++i) {
       if (prices[i] == prices[i - 1] - 1)
         ++dp;
       else

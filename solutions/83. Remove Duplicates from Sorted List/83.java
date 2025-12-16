cff105eb5dfe445fa9d9fc2727f24f111152d4e4
Priyangshu Nath <priyangshu.general00@gmail.com>
2025-12-16 14:07:25 +0530

    Update 83.java
    The previous solution exceeded the time limit.

--- solutions/83. Remove Duplicates from Sorted List/83.java
+++ solutions/83. Remove Duplicates from Sorted List/83.java
@@ -1,13 +1,14 @@
 class Solution {
-  public ListNode deleteDuplicates(ListNode head) {
-    ListNode curr = head;
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
 
-    while (curr != null) {
-      while (curr.next != null && curr.val == curr.next.val)
-        curr.next = curr.next.next;
-      curr = curr.next;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
     }
-
-    return head;
-  }
 }
